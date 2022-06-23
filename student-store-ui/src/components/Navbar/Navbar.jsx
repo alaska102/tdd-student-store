7da25f4872b7819075dd9d9a6066fbf76c31b26d
alaska102 <68394076+alaska102@users.noreply.github.com>
2022-06-23 13:02:26 -0700

    nav home redirect functionality

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import "./Navbar.css";
 import Logo from "../Logo/Logo";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-content">
         <Logo />
-
-        <p className="nav-link">Home</p>
+        <p className="nav-link"><Link to="/" style={{ textDecoration: 'none' , color: 'white'}}>Home</Link></p>
         <p className="nav-link">About Us</p>
         <p className="nav-link">Contact Us</p>
         <p className="nav-link">Buy Now</p>
